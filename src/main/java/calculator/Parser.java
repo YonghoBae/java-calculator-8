e06c7f2028eb2e895b5f3b9ef75ca75bf7882174
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-16 01:30:31 +0900

    feat(parser): 커스텀 구분자 파싱 기능 구현

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
     public int[] parseByDefaultSeparator(String input) {
         String[] textNumbers = input.split("[,:]");
@@ -11,5 +14,29 @@ public class Parser {
         }
 
         return numbers;
+    }
+
+    public int[] parseByCustomSeparator(String input) {
+        Pattern pattern = Pattern.compile("^//(.)\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+
+        if (matcher.find()) {
+            String customSeparator = matcher.group(1);
+            String numberSegment = matcher.group(2);
+
+
+            String[] textNumbers = numberSegment.split(Pattern.quote(customSeparator));
+
+
+            int[] numbers = new int[textNumbers.length];
+
+            for(int i = 0; i < textNumbers.length; i++) {
+                numbers[i] = Integer.parseInt(textNumbers[i]);
+            }
+
+            return numbers;
+        }
+
+        return parseByDefaultSeparator(input);
     }
 }
