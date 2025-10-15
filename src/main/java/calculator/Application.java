db619f893a667899e9dbeaf954fb02c57ad1c4e6
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-16 00:13:44 +0900

    feat(calculator): 숫자 리스트의 합 계산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,5 +3,7 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(new int[]{1,2,3}));
     }
 }
---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+public class Calculator {
+    public int add(int[] numbers){
+        int sum = 0;
+
+        for(int number : numbers){
+            sum += number;
+        }
+
+        return sum;
+    }
+}
