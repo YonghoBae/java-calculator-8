cd95f93c1fc5593368a12d8e5cdec4e26bb220b0
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-17 04:54:28 +0900

    feat(parser): 숫자가 아닌 문자열 입력 시 예외 처리 추가

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -7,6 +7,10 @@ public class Parser {
     public int[] parseByDefaultSeparator(String input) {
         String[] textNumbers = input.split("[,:]");
 
+        if (!isNumberic(textNumbers)) {
+            throw new IllegalArgumentException("입력값에 숫자가 아닌 문자가 포함되어 있습니다.");
+        }
+
         int[] numbers = new int[textNumbers.length];
 
         for(int i = 0; i < textNumbers.length; i++) {
@@ -17,6 +21,7 @@ public class Parser {
     }
 
     public int[] parseByCustomSeparator(String input) {
+
         Pattern pattern = Pattern.compile("^//(.)\n(.*)");
         Matcher matcher = pattern.matcher(input);
 
@@ -24,9 +29,11 @@ public class Parser {
             String customSeparator = matcher.group(1);
             String numberSegment = matcher.group(2);
 
-
             String[] textNumbers = numberSegment.split(Pattern.quote(customSeparator));
 
+            if (!isNumberic(textNumbers)) {
+                throw new IllegalArgumentException("입력값에 숫자가 아닌 문자가 포함되어 있습니다.");
+            }
 
             int[] numbers = new int[textNumbers.length];
 
@@ -38,5 +45,18 @@ public class Parser {
         }
 
         return parseByDefaultSeparator(input);
+    }
+
+    private boolean isNumberic(String[] numbers) {
+        for(String number:numbers){
+            int numberLength = number.length();
+            for(int i=0;i<numberLength;i++){
+                if(!Character.isDigit(number.charAt(i))){
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
