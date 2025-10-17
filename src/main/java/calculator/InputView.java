a56de79bba7fefa271d2e0008aba709691026f92
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-18 01:59:46 +0900

    feat(inputview): 안내 문구 출력 및 사용자 입력 기능 추가

---
+++ src/main/java/calculator/InputView.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String inputStringForSum(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String strForSum = Console.readLine();
+
+        if(strForSum.startsWith("//")){
+            strForSum += Console.readLine();
+        }
+
+        Console.close();
+
+        return strForSum;
+    }
+}
