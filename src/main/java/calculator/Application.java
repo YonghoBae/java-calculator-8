a24f02e0b4d16c4c8985931e6b329262b9b10698
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-18 02:02:16 +0900

    feat(application): 애플리케이션 전체 흐름 통합 및 완성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,12 +3,17 @@ package calculator;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
         Parser parser = new Parser();
         Calculator calculator = new Calculator();
+        OutputView outputView = new OutputView();
 
-        int[] numbers = parser.parseByDefaultSeparator("1,2:3");
+        String input = inputView.inputStringForSum();
+
+        int[] numbers = parser.parseByCustomSeparator(input);
+
         int sum = calculator.add(numbers);
 
-        System.out.println(sum);
+        outputView.print(sum);
     }
 }
